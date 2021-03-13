1b44ed40ef2d2ffc449ea2c09b0ae3acc7a2a3f4
frankgamas45 <frankgamas45@gmail.com>
2021-03-12 19:07:07 -0600

    revisions

--- app/src/main/java/com/frank/flashcardsapp/MainActivity.java
+++ app/src/main/java/com/frank/flashcardsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.frank.flashcardsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -28,5 +29,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        findViewById(R.id.add_Btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && data != null) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("key1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("key2");
+            TextView flashcardQuestion = findViewById(R.id.question);
+            TextView flashcardAnswer = findViewById(R.id.answer);
+            flashcardQuestion.setVisibility(View.VISIBLE);
+            flashcardAnswer.setVisibility(View.INVISIBLE);
+            flashcardQuestion.setText(string1);
+            flashcardAnswer.setText(string2);
+        }
+        else{
+            TextView flashcardQuestion = findViewById(R.id.question);
+            TextView flashcardAnswer = findViewById(R.id.answer);
+            flashcardQuestion.setVisibility(View.VISIBLE);
+            flashcardAnswer.setVisibility(View.INVISIBLE);
+        }
     }
 }
